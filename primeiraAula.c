a27cad42165a2a6a6cfc73ec7f0f35ba713f28d7
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-02-19 16:06:24 +0000

    seg fault

--- primeiraAula.c
+++ primeiraAula.c
@@ -1,23 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(){
 
-    FILE *clientes = fopen("Clientes.txt" , "r");
+    char *listaProdutos[200000];
+    char *listaClientes[20000];
+    char *listaVendas[100000]; // não dá para criar com 1 milhão
+
+    char *strClientes;
+    char *strProdutos;
+    char *strVendas; // Usar depois tamanho da linha maior.
+
+    FILE *clientes = fopen("Clientes.txt", "r");
     FILE *produtos = fopen("Produtos.txt" , "r");
-    FILE *vendas   = fopen("Vendas_1M.txt" , "r");
+    FILE *vendas = fopen("Vendas.txt" , "r");
 
-    char* listaClientes[20000];
-    char* listaProdutos[200000];
-    char* listaVendas[1000000];
-
-    char strClientes[5];
-    char strProdutos[6];
-    char strVendas[1024]; // Usar depois tamanho da linha maior.
-
-    for(int i = 0; i < 20000; i++){
-        fgets(strClientes,5,clientes);
+    for(int i = 0; clientes != NULL && i < 20000; i++){
+            fgets(strClientes,5,clientes);
+            
+            strcpy(listaClientes[i],strClientes); // Segmentation fault aqui
+        
+        printf("%s\n",listaClientes[i]);
     }
 
     fclose(clientes);
     fclose(produtos);
     fclose(vendas);
 
+    return 0;
 }
