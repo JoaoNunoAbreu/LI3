5eb9bac6868f25dd44817419468522fc04e0a351
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-02-20 15:52:33 +0000

    Imprime até 10131 elementos e depois da seg fault

--- primeiraAula.c
+++ primeiraAula.c
@@ -4,29 +4,27 @@
 
 int main(){
 
-    char *listaProdutos[200000];
-    char *listaClientes[20000];
-    char *listaVendas[100000]; // não dá para criar com 1 milhão
+    char listaClientes[20000][5];
+    //char *listaProdutos[200000];
+    //char *listaVendas[100000]; // não dá para criar com 1 milhão
 
-    char *strClientes;
-    char *strProdutos;
-    char *strVendas; // Usar depois tamanho da linha maior.
+    char strClientes[5];
+    //char *strProdutos;
+    //char *strVendas; // Usar depois tamanho da linha maior.
 
     FILE *clientes = fopen("Clientes.txt", "r");
-    FILE *produtos = fopen("Produtos.txt" , "r");
-    FILE *vendas = fopen("Vendas.txt" , "r");
+    //FILE *produtos = fopen("Produtos.txt" , "r");
+    //FILE *vendas = fopen("Vendas.txt" , "r");
 
-    for(int i = 0; clientes != NULL && i < 20000; i++){
-            fgets(strClientes,5,clientes);
-            
-            strcpy(listaClientes[i],strClientes); // Segmentation fault aqui
-        
-        printf("%s\n",listaClientes[i]);
+    for(int i = 0;fgets(strClientes,5,clientes); i++){
+        //printf("%s", strClientes);
+        strcpy(listaClientes[i],strClientes);
+        printf("%s", listaClientes[i]);
     }
 
     fclose(clientes);
-    fclose(produtos);
-    fclose(vendas);
+    //fclose(produtos);
+    //fclose(vendas);
 
     return 0;
 }
