89cbbfd1973c9a2c3882e5f52dae858aa89dd782
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-02-20 18:47:12 +0000

    Já guarda os produtos na lista (tbm imprime no terminal)

--- primeiraAula.c
+++ primeiraAula.c
@@ -1,17 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-int main(){
+int main () {
 
-    char listaClientes[40000][5];
+    //listas
+    char listaProdutos[200000][5];
+    //char *listaClientes[20000];
+    //char *listaVendas[1000000];
 
-    FILE *clientes = fopen("Clientes.txt", "r");
+    FILE *product = fopen("Produtos.txt","r");
+    //FILE *client = fopen("Clientes.txt","r");
+    //FILE *vendas = fopen("Vendas.txt","r");
 
-    for (int i = 0;fgets(listaClientes[i],sizeof listaClientes[i],clientes); i++)
-        printf("%s", listaClientes[i]);
 
-    fclose(clientes);
+
+    // Guarda Produtos
+    for (int i = 0;fgets(listaProdutos[i],sizeof listaProdutos,product);i++){
+        printf("%s", listaProdutos[i]);
+    }
+    printf("\n");
+
+    fclose(product);
+    //fclose(client);
+    //fclose(vendas);
 
     return 0;
+
 }
