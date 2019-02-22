c9b455817ea9359fb7236ad49141f9a9d89525c1
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-02-22 23:02:17 +0000

    Funções de guardar nas listas seperadas da main (vendas ainda por fazer)

--- primeiraAula.c
+++ primeiraAula.c
@@ -2,28 +2,57 @@
 #include <stdio.h>
 #include <string.h>
 
+// ------------ Estas 2 funções são muito semelhantes, provavelmente dá para fazer tudo numa ------
+
+// Guarda os produtos e retorna a quantidade de linhas lidas.
+int guardaProdutos(char listaProdutos[200000][6],FILE *produto){
+
+    int i;
+
+    for (i = 0;fgets(listaProdutos[i],8,produto);i++) // 8 é o valor mínimo que me dá bem
+        printf("%s", listaProdutos[i]); // Não é necessário dar print, é só para ver se está a fazer corretamente
+    printf("\n"); // Efeitos estética no terminal
+    return i;
+}
+
+// Guarda os clientes e retorna a quantidade de linhas lidas.
+int guardaClientes(char listaClientes[20000][5], FILE *clientes){
+
+    int i;
+
+    for (i = 0;fgets(listaClientes[i],7,clientes);i++) // 7 é o valor mínimo que me dá bem
+        printf("%s", listaClientes[i]); // Não é necessário dar print, é só para ver se está a fazer corretamente
+    printf("\n"); // Efeitos estética no terminal
+    return i;
+}
+
+// ------------------------------------------------------------------------------------------------
+
 int main () {
 
-    //listas
-    char listaProdutos[200000][5];
-    //char *listaClientes[20000];
-    //char *listaVendas[1000000];
+    // ----------------- Listas -----------------
+    char listaProdutos[200000][6];
+    char listaClientes[20000][5];
+    //char listaVendas[1000000][100]; // dá seg fault porque array é demasiado grande
 
-    FILE *product = fopen("Produtos.txt","r");
-    //FILE *client = fopen("Clientes.txt","r");
-    //FILE *vendas = fopen("Vendas.txt","r");
+    // --------- Abertura dos ficheiros ---------
+    FILE *produto = fopen("Produtos.txt","r");
+    FILE *clientes = fopen("Clientes.txt","r");
+    FILE *vendas = fopen("Vendas.txt","r");
 
+    // ----------- Guarda nas listas ------------
 
+    //int r1 = guardaProdutos(listaProdutos,produto);
+    //printf("Foram lidas %d linhas do ficheiro produtos.txt\n",r1);
 
-    // Guarda Produtos
-    for (int i = 0;fgets(listaProdutos[i],sizeof listaProdutos,product);i++){
-        printf("%s", listaProdutos[i]);
-    }
-    printf("\n");
+    int r2 = guardaClientes(listaClientes,clientes);
+    printf("Foram lidas %d linhas do ficheiro clientes\n",r2);
+    
+    // ---------- Close dos ficheiros -----------
 
     fclose(product);
-    //fclose(client);
-    //fclose(vendas);
+    fclose(client);
+    fclose(vendas);
 
     return 0;
 
