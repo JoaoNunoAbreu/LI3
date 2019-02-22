bfd17e678b5899137118d9cb5c1fb7e5faabe645
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-02-22 23:13:33 +0000

    Funções de guardar nas listas seperadas da main (vendas ainda por fazer) e algumas notas no fim do ficheiro

--- primeiraAula.c
+++ primeiraAula.c
@@ -57,3 +57,27 @@ int main () {
     return 0;
 
 }
+
+/*
+    NOTA:
+
+    Validade de produtos (AB1234)
+        - 2 letras maiúsculas
+        - 4 dígitos entre 1000 e 9999
+        - Sem repetidos (acho)
+
+    Validade de clientes (A1234)
+        - 1 letra maiúscula
+        - 4 dígitos entre 1000 e 5000
+        - Sem repetidos (acho)
+
+    Validade de vendas (AB1234 123.12 123 N A1234 1 1)
+        - Código de produto
+        - Preço por unidade decimal entre 0.0 e 999.99
+        - Número de unidades compradas entre 1 e 200
+        - Compra normal ou promoção (N ou P)
+        - Código do cliente
+        - Mês da compra
+        - Filial entre 1 e 3
+        - Sem repetidos (acho)
+*/
